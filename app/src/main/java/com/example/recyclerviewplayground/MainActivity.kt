d345bf358e5144380ae7b58566a0c3ceb41dd9e9
Khalid <khalidwork050@outlook.com>
2022-10-14 11:25:01 +0400

    add space with divider

--- app/src/main/java/com/example/recyclerviewplayground/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewplayground/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.recyclerviewplayground
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.example.recyclerviewplayground.databinding.ActivityMainBinding
+import com.example.recyclerviewplayground.recycler_item_decoration.RecyclerViewItemDecoration
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -15,10 +17,23 @@ class MainActivity : AppCompatActivity() {
         setRecyclerView()
     }
 
-    fun setRecyclerView(){
+    private fun setRecyclerView(){
         val adapter = PlayGroundRecyclerViewAdapter()
         binding.homeRv.adapter = adapter
         adapter.submitList(MockModel.DataList)
+
+        binding.homeRv.setSpacingOfEditOrderAdapter()
+    }
+
+    private fun RecyclerView.setSpacingOfEditOrderAdapter() {
+        RecyclerViewItemDecoration(
+            resId = R.drawable.line_divider,
+            context = this@MainActivity,
+            size = resources.getDimension(R.dimen._30dp).toInt(),
+            edgeEnabled = false
+        ).apply {
+            addItemDecoration(this)
+        }
     }
 
 }
